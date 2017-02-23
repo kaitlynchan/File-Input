4473a0526ee6d7b535e96cbe9f3ec887fdee289e
Kaitlyn Chan <ununoctium829@gmail.com>
2017-02-22 22:10:53 -0800

    updates

--- src/FileInput.java
+++ src/FileInput.java
@@ -6,12 +6,12 @@ public class FileInput {
 	public static void main(String[] args) throws FileNotFoundException{
 		Scanner input = new Scanner (new File ("./weather.txt"));
 		
-		double temp1 = input.nextDouble();
-		double temp2 = temp1;
+		double temp2 = input.nextDouble();
 		while (input.hasNextDouble()){
 		
-			temp1 = input.nextDouble();
+			double temp1 = input.nextDouble();
 			System.out.println("change = " + (temp1-temp2));
+			temp2 = temp1;
 		}
 		input.close();
 
